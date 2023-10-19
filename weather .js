4e987e9dc5824a51ae487a224cccb5d110f44ce9
abhibagwan708998 <145115268+abhibagwan708998@users.noreply.github.com>
2023-10-19 16:38:59 +0530

    Update weather .js

--- weather .js
+++ weather .js
@@ -1,36 +1,3 @@
-// const apiurl = "https://api.openweathermap.org/data/2.5/weather?q="
-// const apikey = "6bea00608b0f4483591e3650476e9ee9"
-
-// const searchBox=document.querySelector(".search input")
-// const searchBtn=document.querySelector(".search btn")
-
-// async function checkWeather(city) {
-// const response = await fetch(apiUrl + mhow + `&appid=${apiKey}`);
-// const data = await response.json();
-
-// console.log(data);
-// document.querySelector(".weather h1").innerHTML=Math.round(data.main.temp) = "°C"
-// document.querySelector(".humindity").innerHTML= data.main.humindity = "%"
-// document.querySelector(".wind").innerHTML=data.wind.speed + "km/h"
-// document.querySelector(".city").innerHTML=data.name
-
-// }
-// searchBtn.addEventListener("click",()=>{
-//     checkWeather(searchBox.value);
-// })
-
-// function convertCelsiusToFahrenheit() {
-//     const celsiusElement = document.querySelector(".temp");
-//     const celsiusText = celsiusElement.textContent;
-//     const celsius = parseFloat(celsiusText);
-    
-   
-//       const fahrenheit = (celsius * 9/5) + 32;
-//       document.querySelector(".f").innerHTML = `${fahrenheit} F`;
-//   }
-  
-
-
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 const apiKey = '6bea00608b0f4483591e3650476e9ee9';
 const searchBox=document.querySelector(".search input")
